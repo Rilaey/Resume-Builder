a22dec950970c731773aa05f2e5db884426af431
Riley <94302477+Rilaey@users.noreply.github.com>
2023-05-03 21:06:17 -0400

    fixed error that wouldn't pass data from each inquirer prompt which resulted in undefined values on generated resume. cleaned up and commented code.

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ import projectQuestions from "./questions/projectQuestions.js";
 import experienceQuestions from "./questions/experienceQuestions.js";
 import educationQuestions from "./questions/educationQuestions.js";
 import generateMyResume from "./template/script.js";
-import allQuestions from "./questions/allQuestions.js";
 import fs from "fs";
+
+// shorthand for console.log()
 const log = console.log;
 
 // obv a cool console log to start the application
@@ -16,8 +17,9 @@ log(chalk.red("\\\\\\\\RESUME\\\\\\\\\\\\\\\\"));
 log(chalk.red("\\\\\\\\\\\\BUILDER\\\\\\\\\\\\\\"));
 log(chalk.red("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"));
 
+// write file function
 const startApp = (data) => {
-  fs.writeFile("test.html", data, (err) => {
+  fs.writeFile("./template/generatedResume.html", data, (err) => {
     if (err) {
       console.log(err);
     } else {
@@ -27,12 +29,18 @@ const startApp = (data) => {
 };
 
 // resume input questions
-const askQuestions = async function () {
-    await inquirer.prompt(allQuestions)
-}.then((data) => {
-  console.log(data);
-  let fileName = generateMyResume(data);
-  startApp(fileName);
-});
+const askQuestions = function () {
+  inquirer
+    .prompt(baseQuestions)
+    .then((data) => inquirer.prompt(projectQuestions, data))
+    .then((data) => inquirer.prompt(experienceQuestions, data))
+    .then((data) => inquirer.prompt(educationQuestions, data))
+    .then((data) => {
+      console.log(data);
+      let fileName = generateMyResume(data);
+      startApp(fileName);
+    });
+};
 
+// start the application
 askQuestions();
--- questions/educationQuestions.js
+++ questions/educationQuestions.js
@@ -5,7 +5,7 @@ const educationQuestions = [
   {
     type: "input",
     name: "major",
-    message: "What was your major in college?",
+    message: chalk.yellow("What was your major in college?"),
     validate: (major) => {
       if (!major) {
         log(chalk.red("/// PLEASE INPUT YOUR COLLEGE COLLEGE ///"));
@@ -17,7 +17,7 @@ const educationQuestions = [
   {
     type: "input",
     name: "universityName",
-    message: "What was the name of your university",
+    message: chalk.yellow("What was the name of your university"),
     validate: (universityName) => {
       if (!universityName) {
         log(chalk.red("/// PLEASE INPUT YOUR UNIVERSITY NAME ///"));
@@ -29,7 +29,7 @@ const educationQuestions = [
   {
     type: "input",
     name: "universityCity",
-    message: "What city was your university located?",
+    message: chalk.yellow("What city was your university located?"),
     validate: (universityCity) => {
       if (!universityCity) {
         log(chalk.red("/// PLEASE INPUT YOUR UNIVERSITY CITY ///"));
@@ -41,7 +41,7 @@ const educationQuestions = [
   {
     type: "input",
     name: "universityState",
-    message: "What state was your university located?",
+    message: chalk.yellow("What state was your university located?"),
     validate: (universityState) => {
       if (!universityState) {
         log(chalk.red("/// PLEASE INPUT YOUR UNIVERSITY STATE ///"));
@@ -53,7 +53,7 @@ const educationQuestions = [
   {
     type: "input",
     name: "universitySummary",
-    message: "Please write a summary of your education experience.",
+    message: chalk.yellow("Please write a summary of your education experience."),
     validate: (universitySummary) => {
       if (!universitySummary) {
         log(chalk.red("/// PLEASE INPUT YOUR UNIVERSITY SUMMARY ///"));
